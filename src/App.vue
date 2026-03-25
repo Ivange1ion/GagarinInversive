bef93c184e5e4c589ed17de93e57f04c2b3ed8fc
Kirill Mineev <kirial.adin@yandex.ru>
2026-03-25 22:24:22 +0300

    Add adaptive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import Header from "./components/Header/Header.vue";
+import Hero from "./components/Hero/Hero.vue";
+import Modal from "./components/UI/Modal.vue";
 </script>
 
 <template>
-  <Header></Header>
+  <Header><Modal></Modal></Header>
+  <Hero></Hero>
 </template>
--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,5 +1,73 @@
-<script setup></script>
+<script setup>
+import Button from "../UI/Button.vue";
+</script>
 
-<template></template>
+<template>
+  <div class="container">
+    <h1 class="title">
+      <span class="span">Гагарин</span> Иммерсив - космос начинается здесь
+    </h1>
+    <Button class="hero-btn">Узнать больше</Button>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  margin-left: -30px;
+  margin-right: -35px;
+  margin-top: -30px;
+
+  min-height: 200vh;
+}
+
+.container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url("../../assets/img/hero-bg.svg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  z-index: -1;
+}
+
+.title {
+  color: var(--text);
+  font-size: 67px;
+  font-family: var(--sans);
+  text-align: center;
+  max-width: 915px;
+  margin-bottom: 88px;
+
+  .span {
+    color: var(--text-span);
+  }
+}
+
+@media (max-width: 1200px) {
+  .title {
+    font-size: 57px;
+  }
+}
+
+@media (max-width: 958px) {
+  .title {
+    font-size: 47px;
+  }
+}
+
+@media (max-width: 720px) {
+  .title {
+    font-size: 37px;
+  }
+}
+</style>
