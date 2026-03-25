bd71c0341c9c82d2aec0960cba7c26be4452b8f5
Kirill Mineev <kirial.adin@yandex.ru>
2026-03-25 22:56:26 +0300

    Add layout to Poster component

--- src/components/Poster/Poster.vue
+++ src/components/Poster/Poster.vue
@@ -1,5 +1,47 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <section class="container">
+    <picture class="poster-pic">
+      <img src="../../assets/img/poster.webp" alt="" class="poster-img" />
+    </picture>
 
-<style scoped></style>
+    <div class="text-content">
+      <p class="text-item">Переживите первый полёт в космос!</p>
+      <p class="text-item">
+        Наш проект даёт возможность не просто посмотреть о первом полёте в
+        космос, а очутиться там в виртуальной реальности.
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+}
+
+.poster-pic {
+  display: flex;
+  /* width: 545px; */
+  height: 594px;
+  margin-right: 70px;
+}
+.poster-img {
+  width: 545px;
+  object-fit: cover;
+}
+
+.text-content {
+  align-items: center;
+  justify-content: center;
+}
+
+.text-item {
+  color: #fff;
+  font-size: 50px;
+  font-family: var(--sans);
+  font-weight: 700;
+}
+</style>
