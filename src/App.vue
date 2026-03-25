9665300e26dc88423949d2f52a2e2a06a85545fd
Kirill Mineev <kirial.adin@yandex.ru>
2026-03-25 22:54:44 +0300

    Add new component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from "./components/Header/Header.vue";
 import Hero from "./components/Hero/Hero.vue";
+import Poster from "./components/Poster/Poster.vue";
 import Modal from "./components/UI/Modal.vue";
 </script>
 
 <template>
   <Header><Modal></Modal></Header>
   <Hero></Hero>
+  <Poster></Poster>
 </template>
