f5e793316f02b78356e8c7cc089086254c98e790
Kirill Mineev <kirial.adin@yandex.ru>
2026-03-25 22:23:52 +0300

    Create UI components

--- src/components/UI/Button.vue
+++ src/components/UI/Button.vue
@@ -1,5 +1,41 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <button class="btn"><slot></slot></button>
+</template>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  background-color: #ff8d28;
+  color: black;
+  padding: 25px 250px;
+  border-radius: 60px;
+  text-align: center;
+  font-size: 50px;
+  cursor: pointer;
+}
+
+@media (max-width: 1200px) {
+  .btn {
+    font-size: 40px;
+    padding-left: 200px;
+    padding-right: 200px;
+  }
+}
+
+@media (max-width: 958px) {
+  .btn {
+    font-size: 30px;
+    padding-left: 150px;
+    padding-right: 150px;
+  }
+}
+
+@media (max-width: 720px) {
+  .btn {
+    font-size: 25px;
+    padding-left: 110px;
+    padding-right: 110px;
+  }
+}
+</style>
